e888ab17482a058d31364b6f2530b397d2305bae
Tanmay Sinha <tanmay3011@gmail.com>
2016-04-28 15:08:36 +0530

    Change namespace of Page tracker due to conflict between Page model in spree_static_content

--- app/trackers/spree/page/event/tracker.rb
+++ app/trackers/spree/page/event/tracker.rb
@@ -1,18 +1,16 @@
 module Spree
-  module Page
-    module Event
-      class Tracker < Spree::Event::Tracker
-        EVENTS = { show: :view, search: :search, filter: :filter, index: :index }
+  module Page::Event
+    class Tracker < Spree::Event::Tracker
+      EVENTS = { show: :view, search: :search, filter: :filter, index: :index }
 
-        def initialize(arguments = {})
-          super(arguments)
-          @search_keywords = arguments[:search_keywords]
-          @query_string = arguments[:query_string]
-        end
+      def initialize(arguments = {})
+        super(arguments)
+        @search_keywords = arguments[:search_keywords]
+        @query_string = arguments[:query_string]
+      end
 
-        def track
-          PageEvent.create(instance_values)
-        end
+      def track
+        PageEvent.create(instance_values)
       end
     end
   end
