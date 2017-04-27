003840d735dc813415a116f9bef29855de25e998
kamil-z-hubstyle <kamil.zielinski@hubstyle.pl>
2017-04-27 16:20:29 +0200

    Update order_contents_decorator.rb
    `changed?` doesn't work with `update_attributes` :/

--- app/models/concerns/spree/order_contents_decorator.rb
+++ app/models/concerns/spree/order_contents_decorator.rb
@@ -4,7 +4,7 @@ module Spree
     # Override: since order's line_items were overridden
     def update_cart(params)
       if order.update_attributes(filter_order_items(params))
-        order.line_items.select(&:changed?).each do |line_item|
+        order.line_items.each do |line_item|
           if line_item.previous_changes.keys.include?('quantity')
             Spree::Cart::Event::Tracker.new(
               actor: order, target: line_item, total: order.total, variant_id: line_item.variant_id
